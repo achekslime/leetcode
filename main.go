fc9bef530169f1e4566bd7fbf7026954a8fba035
Asatiani Timur <impsface@yandex.ru>
2024-05-20 19:14:44 +0300

    ref

--- main.go
+++ main.go
@@ -1,19 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"leetcode/solution"
 	"leetcode/utils"
-	"os"
 )
-
-func initBuffers() (*bufio.Reader, *bufio.Writer) {
-	var in *bufio.Reader
-	var out *bufio.Writer
-	in = bufio.NewReader(os.Stdin)
-	out = bufio.NewWriter(os.Stdout)
-	return in, out
-}
 
 func main() {
 	console := utils.NewReaderWriter()
