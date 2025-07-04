3261e973acf45f9c780f18c1bc321d937ccf3587
Asatiani Timur <impsface@yandex.ru>
2025-07-04 22:41:42 +0300

    problem easy: 121. Best Time to Buy and Sell Stock

--- problems/easy/121. Best Time to Buy and Sell Stock.go
+++ problems/easy/121. Best Time to Buy and Sell Stock.go
@@ -2,18 +2,12 @@ package easy
 
 func MaxProfit(prices []int) int {
 	n := len(prices)
-	min, max := prices[0], prices[0]
+	lowest := prices[0]
 	ans := 0
 	for i := 1; i < n; i++ {
-		if prices[i] > max {
-			max = prices[i]
-			if max-min > ans {
-				ans = max - min
-			}
-		}
-		if prices[i] < min {
-			min = prices[i]
-			max = prices[i]
+		ans = max(ans, prices[i]-lowest)
+		if prices[i] < lowest {
+			lowest = prices[i]
 		}
 	}
 	return ans
