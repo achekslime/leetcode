5778bdd15539065cbd905a78288da85dad9acd6f
Asatiani Timur <impsface@yandex.ru>
2025-07-09 16:57:08 +0300

    easy: 2215. Find the Difference of Two Arrays

--- problems/easy/2215. Find the Difference of Two Arrays.go
+++ problems/easy/2215. Find the Difference of Two Arrays.go
@@ -1,40 +1,26 @@
 package easy
 
 func findDifference(nums1 []int, nums2 []int) [][]int {
-	n, m := len(nums1), len(nums2)
+	set1 := make(map[int]struct{}, len(nums1))
+	set2 := make(map[int]struct{}, len(nums2))
 
-	mp1 := make(map[int]struct{}, n)
 	for _, v := range nums1 {
-		mp1[v] = struct{}{}
+		set1[v] = struct{}{}
 	}
-	mp2 := make(map[int]struct{}, m)
 	for _, v := range nums2 {
-		mp2[v] = struct{}{}
+		set2[v] = struct{}{}
 	}
 
-	mpAns1 := make(map[int]struct{})
-	mpAns2 := make(map[int]struct{})
 	ans := make([][]int, 2)
-	for _, v := range nums1 {
-		if _, ok := mp2[v]; ok {
-			continue
+	for key, _ := range set1 {
+		if _, ok := set2[key]; !ok {
+			ans[0] = append(ans[0], key)
 		}
-		if _, ok := mpAns1[v]; ok {
-			continue
-		}
-		ans[0] = append(ans[0], v)
-		mpAns1[v] = struct{}{}
 	}
-	for _, v := range nums2 {
-		if _, ok := mp1[v]; ok {
-			continue
+	for key, _ := range set2 {
+		if _, ok := set1[key]; !ok {
+			ans[1] = append(ans[1], key)
 		}
-		if _, ok := mpAns2[v]; ok {
-			continue
-		}
-		ans[1] = append(ans[1], v)
-		mpAns2[v] = struct{}{}
 	}
-
 	return ans
 }
